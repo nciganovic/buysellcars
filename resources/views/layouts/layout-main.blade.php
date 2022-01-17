1a2c38601f49d87a540fa622b911bb3d0833c325
nciganovic <nciganovic99@gmail.com>
2022-01-17 22:23:31 +0100

    Remove default view

---
+++ resources/views/layouts/layout-main.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('includes.head')
+</head>
+<body>
+   
+    @yield('content')
+    @yield('scripts')
+</body>
+</html>
---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.layout-main')
+@section('title')
+    Welcome to Buy&Sell Cars
+@endsection
+@section('content')
+    
+    
+@endsection
+@section('scripts')
+    <script src="{{ asset('js/index.js') }}"></script>
+@endsection
