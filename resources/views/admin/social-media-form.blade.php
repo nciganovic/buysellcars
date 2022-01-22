e82cda06ef432f4e8b1bdfb877b9655c2927ff7a
nciganovic <nciganovic99@gmail.com>
2022-01-22 11:27:48 +0100

    rename page

--- resources/views/admin/social-media-form.blade.php
+++ resources/views/admin/social-media-form.blade.php
@@ -6,16 +6,21 @@
 <div class="container">
     <div class="row">
         <div class="col-12 mt-3">
-            <h1 class="text-center">Create Social Media</h1>
+            <h1 class="text-center">{{ $action }} Social Media</h1>
         </div>
         <div class="d-flex justify-content-center">
             <div class="col-8">
-                <form method="POST" action="{{ route("post_create_admin_social_media") }}">
+                <form method="POST" 
+                    @if($action == "Create")
+                        action="{{ route("post_create_admin_social_media") }}"
+                    @else 
+                        action="{{ route("post_edit_admin_social_media", $model->id) }}"
+                    @endif>
                     @csrf
 
                     <div class="form-group mt-3">    
                         <label for="name">Name</label>
-                        <input class="form-control" id="name" name="name" type="text" class="@error('name') is-invalid @enderror">
+                        <input class="form-control" id="name" name="name" type="text" value="{{ old('name', $model->name) }}" class="@error('name') is-invalid @enderror">
                         @error('name')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
@@ -23,7 +28,7 @@
 
                     <div class="form-group mt-3">    
                         <label for="url">Url</label>
-                        <input class="form-control" id="url" name="url" type="url" class="@error('url') is-invalid @enderror">
+                        <input class="form-control" id="url" name="url" type="text" value="{{ old('url', $model->url) }}" class="@error('url') is-invalid @enderror">
                         @error('url')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
@@ -31,13 +36,17 @@
 
                     <div class="form-group mt-3">    
                         <label for="logo">Logo</label>
-                        <input class="form-control" id="logo" name="logo" type="text" class="@error('logo') is-invalid @enderror">
+                        <input class="form-control" id="logo" name="logo" type="text" value="{{ old('logo', $model->logo) }}" class="@error('logo') is-invalid @enderror">
                         @error('logo')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
                     </div>
 
+                    @if($action == "Create")
                     <button class="mt-3 btn btn-success" type="submit">Create</button>
+                    @else
+                    <button class="mt-3 btn btn-warning" type="submit">Update</button>
+                    @endif
                 </form>
             </div>
         </div>
